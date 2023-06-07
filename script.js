ca1443e6862a99f98fd510842f0e68607b957cbe
Gabriel Saucedo <saucedogabriel99@gmail.com>
2023-06-07 10:57:06 -0300

    [FEAT] modificacion de estilos y correccion de errores de redireccion a inicio

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // Código JavaScript del archivo index.html
 const FORMULARIO = document.querySelector('.section-form');
-const INPUT = document.querySelector('#email');
-const SUBMIT = document.querySelector('.submit');
+const INPUT_EMAIL = document.querySelector('#email');
+const INPUT_SUBMIT = document.querySelector('.submit');
 const MAIN = document.querySelector('#main');
-const INVALID = document.querySelector('.invalid');
+const INVALID_TEXT = document.querySelector('.invalid');
 const INVALID_EMAIL = document.querySelector('.invalid-email');
 
-INVALID.style.display = 'none';
+INVALID_TEXT.style.display = 'none';
 
 const inputFunction = () => {
-    if(INPUT.value.trim() !== ''){
-        INPUT.classList.remove('invalid-input');
-        INVALID.style.display = 'none';
+    if(INPUT_EMAIL.value.trim() !== ''){
+        INPUT_EMAIL.classList.remove('invalid-input');
+        INVALID_TEXT.style.display = 'none';
     }
 }
 
 const submitFunction = () => {
-    const INPUT_USER = INPUT.value.trim();
+    const INPUT_USER = INPUT_EMAIL.value.trim();
      if(FORMULARIO.checkValidity()){
         MAIN.innerHTML = `
         <section class="section-succes">
@@ -28,27 +28,23 @@ const submitFunction = () => {
                         <span>${INPUT_USER}.</span>  Please open it and click 
                          the button inside to confirm your suscription.
                     </p>
-                    <input type="button" value="Dissmis mesagge">
+                    <div class="text-succes-container">
+                        <p class="text-succes-btn">
+                            <a href="./index.html" class="dissmis_button">Dissmis mesagge</a>
+                        </p>
+                    </div>
                 </article>
             </section>
         `
         FORMULARIO.submit();
-    }else if(INPUT.value.trim() !== '' && !form) {
-        INPUT.classList.add('invalid-input');
-        INVALID.style.display = 'block';
-    }else if(INPUT.value.trim() == ''){
-        INPUT.classList.remove('invalid-input');
+    }else if(INPUT_EMAIL.value.trim() !== '' && !FORMULARIO.checkValidity()) {
+        INPUT_EMAIL.classList.add('invalid-input');
+        INVALID_TEXT.style.display = 'block';
+    }else if(INPUT_EMAIL.value.trim() == ''){
+        INPUT_EMAIL.classList.remove('invalid-input');
     }
 }
  
-SUBMIT.addEventListener('click', submitFunction );
-INPUT.addEventListener('input', inputFunction );
+INPUT_SUBMIT.addEventListener('click', submitFunction );
+INPUT_EMAIL.addEventListener('input', inputFunction );
 
-// Código JavaScript del archivo succes.html
-const myButtonDissmis = (e) => {
-    if (e.target.classList.contains('dissmis_button')) {
-        console.log('Diste click en el botón dissmis mesagge');
-    }
-}
-
-MAIN.addEventListener('click', myButtonDissmis);
